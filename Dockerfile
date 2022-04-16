cec5777c1a577d7456e63e3ef40fd592c5498874
Gamid Ogurliev <gogurliev@rbc.ru>
2022-04-16 16:17:56 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,9 @@ ENV PYTHONUNBUFFERED 1
 RUN apk update \
     && apk add postgresql-dev gcc python3-dev musl-dev git
 
-RUN git clone https://github.com/ogurliev02/university_service.git .
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
 RUN pip install --upgrade pip
-RUN pip install -r ./requirements.txt
+COPY . .
 
 ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
--- adm/models.py
+++ adm/models.py
@@ -1,3 +1,33 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    title = models.CharField(verbose_name="Название университета", max_length=255)
+    disciplines = ArrayField(models.CharField(max_length=155), verbose_name="Список факультетов", null=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    university = models.ForeignKey(University, on_delete=models.SET_NULL, null=True)
+    group_number = models.CharField(verbose_name="Номер группы", max_length=255, null=True)
+
+
+class Project(models.Model):
+    users = ArrayField(models.ForeignKey(Profile, on_delete=models.SET_NULL), verbose_name="Список заинтересованных лиц", null=True)
+    project_manager = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Проджект-менеджер", null=True)
+    tester = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Тестировщик", null=True)
+    front_end = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Фронтендер", null=True)
+    back_end = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Бэкендер", null=True)
+    design = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Дизайнер", null=True)
+    mentor = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Куратор проекта", null=True)
+    title = models.CharField(verbose_name="Название проекта", max_length=255, null=False)
+    description = models.CharField(verbose_name="Описание проекта", max_length=255, null=True)
+    goal = models.CharField(verbose_name="Цель проекта", max_length=255, null=False)
+    start_date = models.DateTimeField(verbose_name="Дата начала проекта", default=datetime.datetime.utcnow())
+    deadline_date = models.DateTimeField(verbose_name="Дедлайн проекта", null=False)
+    end_date = models.DateTimeField(verbose_name="Дата конеца проекта", null=True)
+    university = models.ForeignKey(University, on_delete=models.SET_NULL)
