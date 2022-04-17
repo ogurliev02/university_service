8105dda85ac9bb6bb7767f8ae8387efe5a93365f
Gamid Ogurliev <gogurliev@rbc.ru>
2022-04-17 17:11:43 +0300

    Создал модели в базе

--- adm/admin.py
+++ adm/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from adm.models import (
+    Profile,
+    Category,
+    Project,
+    Mark,
+    University,
+)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = [field.name for field in model._meta.fields if field.name != "id"]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = [field.name for field in model._meta.fields if field.name != "id"]
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = [field.name for field in model._meta.fields if field.name != "id"]
+
+
+class MarkAdmin(admin.ModelAdmin):
+    model = Mark
+    list_display = [field.name for field in model._meta.fields if field.name != "id"]
+
+
+class UniversityAdmin(admin.ModelAdmin):
+    model = University
+    list_display = [field.name for field in model._meta.fields if field.name != "id"]
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Mark, MarkAdmin)
+admin.site.register(University, UniversityAdmin)
--- adm/models.py
+++ adm/models.py
@@ -12,22 +12,52 @@ class University(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    university = models.ForeignKey(University, on_delete=models.SET_NULL, null=True)
+    university = models.ForeignKey(University, on_delete=models.SET_NULL, null=True, related_name='user_university')
     group_number = models.CharField(verbose_name="Номер группы", max_length=255, null=True)
 
 
+class Category(models.Model):
+    title = models.CharField(verbose_name="Название категории", max_length=155, null=False)
+    description = models.CharField(verbose_name="Описание категории" ,max_length=255, null=True)
+
+
 class Project(models.Model):
-    users = ArrayField(models.ForeignKey(Profile, on_delete=models.SET_NULL), verbose_name="Список заинтересованных лиц", null=True)
-    project_manager = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Проджект-менеджер", null=True)
-    tester = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Тестировщик", null=True)
-    front_end = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Фронтендер", null=True)
-    back_end = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Бэкендер", null=True)
-    design = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Дизайнер", null=True)
-    mentor = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Куратор проекта", null=True)
+    users = ArrayField(models.IntegerField(), verbose_name="Список заинтересованных лиц")
+    project_manager = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Проджект-менеджер", null=True,
+                                        related_name='project_project_manager')
+    tester = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Тестировщик", null=True,
+                               related_name='project_tester')
+    front_end = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Фронтендер", null=True,
+                                  related_name='project_front_end')
+    back_end = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Бэкендер", null=True,
+                                 related_name='project_back_end')
+    designer = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Дизайнер", null=True,
+                                 related_name='project_designer')
+    mentor = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name="Куратор проекта", null=True,
+                               related_name='project_mentor')
     title = models.CharField(verbose_name="Название проекта", max_length=255, null=False)
     description = models.CharField(verbose_name="Описание проекта", max_length=255, null=True)
     goal = models.CharField(verbose_name="Цель проекта", max_length=255, null=False)
     start_date = models.DateTimeField(verbose_name="Дата начала проекта", default=datetime.datetime.utcnow())
     deadline_date = models.DateTimeField(verbose_name="Дедлайн проекта", null=False)
     end_date = models.DateTimeField(verbose_name="Дата конеца проекта", null=True)
-    university = models.ForeignKey(University, on_delete=models.SET_NULL)
+    university = models.ForeignKey(University, on_delete=models.SET_NULL, verbose_name="Университет", null=True,
+                                   related_name='project_university')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория проекта", null=True,
+                                 related_name='project_category')
+
+
+class MarkChoices(models.IntegerChoices):
+    TOO_BAD = 1, 'Too Bad'
+    BAD = 2, 'Bad'
+    NORMAL = 3, 'Normal'
+    GOOD = 4, 'Good'
+    EXCELLENT = 5, 'Excellent'
+
+
+class Mark(models.Model):
+    mentor = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name="Куратор проекта", null=False,
+                               related_name='mark_mentor')
+    student = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name="Студент", null=False,
+                                related_name='mark_student')
+    mark = models.IntegerField(verbose_name="Оценка", default=MarkChoices.BAD, choices=MarkChoices.choices)
